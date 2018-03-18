f03a4f45d003a7f530e29cf30d046fdd27e0b5e3
srishti <srishti.sengupta@gmail.com>
2018-03-19 01:18:24 +0530

    Added DeviceScanActivity

---
+++ app/src/main/java/com/blesniff/insanegupta/blesniff/DeviceScanActivity.java
@@ -0,0 +1,62 @@
+package com.blesniff.insanegupta.blesniff;
+
+import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.os.Handler;
+
+import java.util.List;
+
+/**
+ * Created by srishti on 3/18/18.
+ */
+
+public class DeviceScanActivity extends ListActivity{
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+
+    //Stop scanning after 10 seconds
+    private static final long SCAN_PERIOD = 10000;
+
+    private ScanCallback mLeScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+        }
+    };
+
+    private void scanBLEDevice(final boolean enable){
+        if(enable){
+            //stop scanning after predefined scan period
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.getBluetoothLeScanner().stopScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.getBluetoothLeScanner().startScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.getBluetoothLeScanner().stopScan(mLeScanCallback);
+
+        }
+    }
+}
--- app/src/main/java/com/blesniff/insanegupta/blesniff/MainActivity.java
+++ app/src/main/java/com/blesniff/insanegupta/blesniff/MainActivity.java
@@ -1,13 +1,34 @@
 package com.blesniff.insanegupta.blesniff;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BluetoothAdapter mBluetoothAdapter;
+    private final static int REQUEST_ENABLE_BT = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialise Bluetooth adapter
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
+
+
 }
